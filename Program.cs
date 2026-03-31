35b78062dc5ba036f622776a8c15bf1a559cc38c
Adam <adam.chmielewski1@gmail.com>
2026-03-31 22:57:21 +0200

    Update greeting message in feature branch

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using ApbdGit;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello from feature branch");
 
 int[] sampleData = { 1, 2, 3, 4, 5 };
 StatisticsHelper helper = new StatisticsHelper();

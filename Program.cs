2b9a15097a97698c491ba18435b972b64973ba72
Adam <adam.chmielewski1@gmail.com>
2026-03-31 22:40:24 +0200

    Integrate StatisticsHelper into main program execution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ApbdGit;
+
+Console.WriteLine("Hello, World!");
+
+int[] sampleData = { 1, 2, 3, 4, 5 };
+StatisticsHelper helper = new StatisticsHelper();
+helper.ProcessData(sampleData);
+
+Console.WriteLine("Dane zostały przetworzone.");

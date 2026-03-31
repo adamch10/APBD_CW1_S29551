9e734a4dc81de52eb0010629299ef0fd5f01d26d
Adam <adam.chmielewski1@gmail.com>
2026-03-31 22:38:41 +0200

    Add basic structure to StatisticsHelper

---
+++ StatisticsHelper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApbdGit
+{
+    public class StatisticsHelper
+    {
+        public void ProcessData(int[] data)
+        {
+            // Miejsce na przyszłą logikę
+        }
+    }
+}
